01e177790b64481c65a691dec86c5a95fba59449
Patrick Connolly <patrick.c.connolly@gmail.com>
2025-07-10 22:14:04 -0400

    Added tab20 color palette.

--- js/config.js
+++ js/config.js
@@ -24,7 +24,36 @@ const Config = {
             "#7f7f7f", // (H) middle gray
             "#bcbd22", // (I) curry yellow-green
             "#17becf", // (J) blue-teal
-        ]
+        ],
+        /*
+            ```py
+            import matplotlib as plt
+            for c in plt.cm.tab20.colors: print(matplotlib.colors.to_hex(c))
+            ```
+        */
+        tab20: [
+            "#1f77b4",
+            "#aec7e8",
+            "#ff7f0e",
+            "#ffbb78",
+            "#2ca02c",
+            "#98df8a",
+            "#d62728",
+            "#ff9896",
+            "#9467bd",
+            "#c5b0d5",
+
+            "#8c564b",
+            "#c49c94",
+            "#e377c2",
+            "#f7b6d2",
+            "#7f7f7f",
+            "#c7c7c7",
+            "#bcbd22",
+            "#dbdb8d",
+            "#17becf",
+            "#9edae5",
+        ],
     },
 
     // Chart colors for vote visualization
